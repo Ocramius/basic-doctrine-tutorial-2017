65b8839dba0840bd50c9a1db9350893afbd8d814
Marco Pivetta <ocramius@gmail.com>
2016-04-23 04:05:33 +0200

    Basic registration feature

---
+++ basic-tutorial/public/register.php
@@ -0,0 +1,10 @@
+<?php
+
+// registering a new user:
+
+// 1. check if a user with the same email address exists
+// 2. if not, create a user
+// 3. send the email to confirm activation (we will just display it)
+// 4. save the user
+
+// Tip: discuss - email or saving? Chicken-egg problem
