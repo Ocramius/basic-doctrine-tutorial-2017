76973f8af3cedeb442b192e0ad8fb42ee07cbe0c
Marco Pivetta <ocramius@gmail.com>
2016-04-23 04:13:43 +0200

    Login page

---
+++ basic-tutorial/public/login.php
@@ -0,0 +1,10 @@
+<?php
+
+// 1. fetch user by email
+// 2. compare user password hash against given password
+// 3. log login (optional)
+// 4. store user identifier into the session
+
+// discuss: should the fetching by password happen at database level?
+//          Should it happen inside the entity?
+//          Or in a service?
