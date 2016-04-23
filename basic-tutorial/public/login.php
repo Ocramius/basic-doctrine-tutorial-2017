f8f8ba02ab691bdc1897ca0f91d9cf51056f9f12
Marco Pivetta <ocramius@gmail.com>
2016-04-23 04:18:08 +0200

    Login should also check whether the user is banned or not

--- basic-tutorial/public/login.php
+++ basic-tutorial/public/login.php
@@ -2,8 +2,9 @@
 
 // 1. fetch user by email
 // 2. compare user password hash against given password
-// 3. log login (optional)
-// 4. store user identifier into the session
+// 3. is the user banned? (optional)
+// 4. log login (optional)
+// 5. store user identifier into the session
 
 // discuss: should the fetching by password happen at database level?
 //          Should it happen inside the entity?
