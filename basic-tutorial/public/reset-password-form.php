a8e8519d9adf9991464130c9e1562f6c8b0345a6
Marco Pivetta <ocramius@gmail.com>
2016-04-24 16:14:07 +0200

    Reset-password outline

---
+++ basic-tutorial/public/reset-password-form.php
@@ -0,0 +1,4 @@
+<form action="reset-password.php" method="post">
+    <input type="email" name="emailAddress" placeholder="Email Address" required="required"/>
+    <input type="submit"/>
+</form>
---
+++ basic-tutorial/public/reset-password.php
@@ -0,0 +1,9 @@
+<?php
+
+// 1. check if a user with the email address exists
+// 2. if not, return a confirmation message to the user (we can't reveal which users exist in our DB!)
+// 3. fetch the user
+// 4. create a password reset link
+// 5. save the user
+
+// Tip: discuss - do we need to save a password reset link?
