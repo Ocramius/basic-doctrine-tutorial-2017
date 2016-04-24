36776d83523f98bf41769d07788b3a115f0a05d8
Marco Pivetta <ocramius@gmail.com>
2016-04-25 00:21:11 +0200

    Endpoint to return books

---
+++ advanced-tutorial/public/return-book.php
@@ -0,0 +1,7 @@
+<?php
+
+// take the authenticated user
+// return a book (amount = 1) to the library
+// make sure the book is in the books that were actually lent
+// bonus: do we allow returning multiple books to be returned at the same time?
+// bonus: do we deal with overdue book returns?
