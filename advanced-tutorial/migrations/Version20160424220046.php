78dc59ef1bc91b200e9fc077205a3501c8a90199
Marco Pivetta <ocramius@gmail.com>
2016-04-25 00:09:47 +0200

    Inserting some dummy users into our DB - useful for auth

--- advanced-tutorial/migrations/Version20160424220046.php
+++ advanced-tutorial/migrations/Version20160424220046.php
@@ -21,6 +21,8 @@ class Version20160424220046 extends AbstractMigration
         );
 
         $this->addSql('CREATE TABLE User (id CHAR(36) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO User (id) VALUES (\'78710367-e5e3-4e44-80fe-5cf885408d1e\')');
+        $this->addSql('INSERT INTO User (id) VALUES (\'8d4f5ffd-257f-496f-a6da-6199ea38439c\')');
     }
 
     /**
