9bad42feafe17581baf4783397e23ad00e2d3dcd
Marco Pivetta <ocramius@gmail.com>
2016-04-24 16:12:21 +0200

    Correcting form action

--- basic-tutorial/public/change-password-form.php
+++ basic-tutorial/public/change-password-form.php
@@ -1,4 +1,4 @@
-<form action="register.php" method="post">
+<form action="change-password.php" method="post">
     <input type="password" name="oldPassword" placeholder="Old Password" required="required"/>
     <input type="password" name="newPassword" placeholder="New Password" required="required"/>
     <input type="submit"/>
