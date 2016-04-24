11a9110cb289050747c425cb52bdbc2f1836f2df
Marco Pivetta <ocramius@gmail.com>
2016-04-24 16:16:57 +0200

    Form to create a new user

---
+++ basic-tutorial/public/create-user-form.php
@@ -0,0 +1,5 @@
+<form action="create-user.php" method="post">
+    <input type="email" name="emailAddress" placeholder="Email Address" required="required"/>
+    <input type="text" name="role" placeholder="Role" required="required"/>
+    <input type="submit"/>
+</form>
