cfdf46f34bd2a0689bc388e48ebc14142b827b5c
Marco Pivetta <ocramius@gmail.com>
2016-04-24 16:09:40 +0200

    Change password form

---
+++ basic-tutorial/public/change-password-form.php
@@ -0,0 +1,5 @@
+<form action="register.php" method="post">
+    <input type="password" name="oldPassword" placeholder="Old Password" required="required"/>
+    <input type="password" name="newPassword" placeholder="New Password" required="required"/>
+    <input type="submit"/>
+</form>
