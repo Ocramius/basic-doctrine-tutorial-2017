09226dbca17d37e33983877f994b7a7078adb72a
Marco Pivetta <ocramius@gmail.com>
2016-04-24 23:56:02 +0200

    Adding connection helper to cli-config

--- advanced-tutorial/cli-config.php
+++ advanced-tutorial/cli-config.php
@@ -1,8 +1,13 @@
 <?php
 
+use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Helper\HelperSet;
 
+/* @var $entityManager \Doctrine\ORM\EntityManagerInterface */
+$entityManager = require __DIR__ . '/bootstrap.php';
+
 return new HelperSet([
-    'em' => new EntityManagerHelper(require __DIR__ . '/bootstrap.php')
+    'em'         => new EntityManagerHelper($entityManager),
+    'connection' => new ConnectionHelper($entityManager->getConnection()),
 ]);
