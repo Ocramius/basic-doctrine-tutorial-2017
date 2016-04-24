857aa641db7739f1f11e1faec08f460c61d36bbb
Marco Pivetta <ocramius@gmail.com>
2016-04-25 00:19:28 +0200

    endpoint to lend a book to a user

---
+++ advanced-tutorial/public/lend-book-to-user.php
@@ -0,0 +1,6 @@
+<?php
+
+// take the authenticated user
+// assign a book (amount = 1) to the user
+// make sure consistency is maintained - can't lend what isn't in inventory at the moment
+// make sure the user is not over-quota with the books he currently has lent from the library
